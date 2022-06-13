e71806648453651591d104dfd2d35317cd05a6fa
Tobias Puszkar <puszkartobias17@gmail.com>
2022-06-13 19:26:10 -0300

    ventajas

--- function.js
+++ function.js
@@ -2,4 +2,15 @@ const menu = document.querySelector('.menu')
 
     window.addEventListener('scroll',function(){
         menu.classList.toggle('active',window.scrollY >0)
-    })    
+    })  
+
+
+    var miNombre = "Tobias";
+    
+    console.log (miNombre);
+
+    var saludo = "hola " + miNombre;
+
+    console.log(saludo);
+
+   
